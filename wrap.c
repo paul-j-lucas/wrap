37087c1230dcc626368c1b03e522eff996480cd6
Paul J. Lucas <paul@lucasmail.org>
2005-07-03 18:33:10 +0000

    Updated copyright notice to include "2005".

--- wrap.c
+++ wrap.c
@@ -2,7 +2,7 @@
 **      wrap -- text reformatter
 **      wrap.c: implementation
 **
-**      Copyright (C) 1996  Paul J. Lucas
+**      Copyright (C) 1996-2005  Paul J. Lucas
 **
 **      This program is free software; you can redistribute it and/or modify
 **      it under the terms of the GNU General Public License as published by
