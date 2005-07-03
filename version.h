87de1d15da8eda1f565c91b94bacadb09ecb3291
Paul J. Lucas <paul@lucasmail.org>
2005-07-03 17:59:14 +0000

    1. Changed to 2.10. 2. Changed tabs to spaces.

--- version.h
+++ version.h
@@ -1,27 +1,28 @@
 /*
-**	wrap -- text reformatter
-**	version.h
+**      wrap -- text reformatter
+**      version.h
 **
-**	Copyright (C) 1996  Paul J. Lucas
+**      Copyright (C) 1996  Paul J. Lucas
 **
-**	This program is free software; you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation; either version 2 of the Licence, or
-**	(at your option) any later version.
+**      This program is free software; you can redistribute it and/or modify
+**      it under the terms of the GNU General Public License as published by
+**      the Free Software Foundation; either version 2 of the Licence, or
+**      (at your option) any later version.
 ** 
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+**      This program is distributed in the hope that it will be useful,
+**      but WITHOUT ANY WARRANTY; without even the implied warranty of
+**      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**      GNU General Public License for more details.
 ** 
-**	You should have received a copy of the GNU General Public License
-**	along with this program; if not, write to the Free Software
-**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**      You should have received a copy of the GNU General Public License
+**      along with this program; if not, write to the Free Software
+**      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#ifndef	version_H
-#define	version_H
+#ifndef version_H
+#define version_H
 
-#define VERSION	"2.9.1"
+#define WRAP_VERSION    "2.10"
 
-#endif	/* C_compat_H */
+#endif  /* C_compat_H */
+/* vim:set et sw=4 ts=4: */
