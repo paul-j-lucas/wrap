6f5ccb51fe482e181bb9ad3fbeb3aab3a59ac779
Paul J. Lucas <paul@lucasmail.org>
2013-11-12 06:07:35 +0000

    Now using GNU test driver mechanism.

---
+++ test/run_test.sh
@@ -0,0 +1,211 @@
+#! /bin/sh
+##
+#       wrap -- text reformatter
+#       test/run_test.sh
+#
+#       Copyright (C) 2013  Paul J. Lucas
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the Licence, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+#set -x
+
+ME=$0; ME=`expr $ME : '.*/\(.*\)'`
+
+if [ -z "$WRAP" ]
+then
+  echo "$ME: error: \$WRAP not set" >&2
+  exit 2
+fi
+
+########## Functions ##########################################################
+
+pass() {
+  print_result PASS $TEST_NAME
+  {
+    echo ":test-result: PASS"
+    echo ":copy-in-global-log: no"
+  } >> $TRS_FILE
+}
+
+fail() {
+  print_result FAIL $TEST_NAME
+  {
+    echo ":test-result: FAIL"
+    echo ":copy-in-global-log: yes"
+  } >> $TRS_FILE
+  exit $ACTUAL_EXIT
+}
+
+print_result() {
+  RESULT=$1; shift
+  MSG="${RESULT_STR[ $RESULT ]}: $*"
+  COLOR=${RESULT_COLOR[ $RESULT ]}
+  if [ "$COLOR" ]
+  then echo $COLOR$MSG$COLOR_NONE
+  else echo $MSG
+  fi
+}
+
+usage() {
+  if [ "$1" ]
+  then
+    echo "$ME: $*" >&2
+    usage
+  fi
+  cat >&2 <<END
+usage: $ME --test-name=NAME --log-file=PATH --trs-file=PATH [options] TEST-COMMAND
+options:
+  --color-tests={yes|no}
+  --enable-hard-errors={yes|no}
+  --expect-failure={yes|no}
+END
+  exit 1
+}
+
+########## Process command-line ###############################################
+
+while [ $# -gt 0 ]
+do
+  case $1 in
+  --color-tests)
+    COLOR_TESTS=$2; shift
+    ;;
+  --color-tests=*)
+    COLOR_TESTS=`expr "x$1" : 'x--color-tests=\(.*\)'`
+    ;;
+  --enable-hard-errors)
+    ENABLE_HARD_ERRORS=$2; shift
+    ;;
+  --enable-hard-errors=*)
+    ENABLE_HARD_ERRORS=`expr "x$1" : 'x--enable-hard-errors=\(.*\)'`
+    ;;
+  --expect-failure)
+    EXPECT_FAILURE=$2; shift
+    ;;
+  --expect-failure=*)
+    EXPECT_FAILURE=`expr "x$1" : 'x--expect-failure=\(.*\)'`
+    ;;
+  --help)
+    usage
+    ;;
+  --log-file)
+    LOG_FILE=$2; shift
+    ;;
+  --log-file=*)
+    LOG_FILE=`expr "x$1" : 'x--log-file=\(.*\)'`
+    ;;
+  --test-name)
+    TEST_NAME=$2; shift
+    ;;
+  --test-name=*)
+    TEST_NAME=`expr "x$1" : 'x--test-name=\(.*\)'`
+    ;;
+  --trs-file)
+    TRS_FILE=$2; shift
+    ;;
+  --trs-file=*)
+    TRS_FILE=`expr "x$1" : 'x--trs-file=\(.*\)'`
+    ;;
+  --)
+    shift
+    break
+    ;;
+  -*)
+    usage
+    ;;
+  *)
+    break
+    ;;
+  esac
+  shift
+done
+
+[ -z "$TEST_NAME" ] && usage "required --test-name not given"
+[ -z "$LOG_FILE"  ] && usage "required --log-file not given"
+[ -z "$TRS_FILE"  ] && usage "required --trs-file not given"
+TEST=$1
+
+########## Initialize #########################################################
+
+rm -f $LOG_FILE $TRS_FILE
+
+PASS=0
+XPASS=1
+FAIL=2
+XFAIL=3
+SKIP=4
+ERROR=5
+
+RESULT_STR[ $ERROR ]="ERROR"
+RESULT_STR[ $FAIL  ]="FAIL"
+RESULT_STR[ $PASS  ]="PASS"
+RESULT_STR[ $SKIP  ]="SKIP"
+RESULT_STR[ $XFAIL ]="XFAIL"
+RESULT_STR[ $XPASS ]="XPASS"
+
+if [ "$COLOR_TESTS" = "yes" ]
+then
+  COLOR_BLUE="[1;34m"
+  COLOR_GREEN="[0;32m"
+  COLOR_LIGHT_GREEN="[1;32m"
+  COLOR_MAGENTA="[0;35m"
+  COLOR_NONE="[m"
+  COLOR_RED="[0;31m"
+
+  RESULT_COLOR[ $ERROR ]=$COLOR_MAGENTA
+  RESULT_COLOR[ $FAIL  ]=$COLOR_RED
+  RESULT_COLOR[ $PASS  ]=$COLOR_GREEN
+  RESULT_COLOR[ $SKIP  ]=$COLOR_BLUE
+  RESULT_COLOR[ $XFAIL ]=$COLOR_LIGHT_GREEN
+  RESULT_COLOR[ $XPASS ]=$COLOR_RED
+fi
+
+case $EXPECT_FAILURE in
+yes) EXPECT_FAILURE=1 ;;
+  *) EXPECT_FAILURE=0 ;;
+esac
+
+DATA_DIR="$srcdir/data"
+EXPECTED_DIR="$srcdir/expected"
+
+########## Run test ###########################################################
+
+IFS='|' read CONFIG OPTIONS INPUT EXPECTED_EXIT < $TEST
+CONFIG=`echo $CONFIG`                   # trims whitespace
+[ "$CONFIG" != /dev/null ] && CONFIG="$DATA_DIR/$CONFIG"
+INPUT="$DATA_DIR/`echo $INPUT`"         # trims whitespace
+EXPECTED_EXIT=`echo $EXPECTED_EXIT`     # trims whitespace
+EXPECTED_OUTPUT="$EXPECTED_DIR/`echo $TEST_NAME | sed 's/test$/txt/'`"
+
+if $WRAP -c $CONFIG $OPTIONS -f $INPUT > $LOG_FILE 2>&1
+then
+  if [ 0 -eq $EXPECTED_EXIT ]
+  then
+    if diff $LOG_FILE $EXPECTED_OUTPUT >/dev/null
+    then pass
+    else fail
+    fi
+  else
+    fail
+  fi
+else
+  ACTUAL_EXIT=$?
+  if [ $ACTUAL_EXIT -eq $EXPECTED_EXIT ]
+  then pass
+  else fail
+  fi
+fi
+
+# vim:set et sw=2 ts=2:
