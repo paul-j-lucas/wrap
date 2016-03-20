3b8afb76bcf3b49dcf3d73cfbf365dc7321fd122
Paul J. Lucas <plucas@splunk.com>
2016-03-20 16:44:12 -0700

    s/2015/2016/

--- src/wrap.c
+++ src/wrap.c
@@ -2,7 +2,7 @@
 **      wrap -- text reformatter
 **      wrap.c
 **
-**      Copyright (C) 1996-2015  Paul J. Lucas
+**      Copyright (C) 1996-2016  Paul J. Lucas
 **
 **      This program is free software; you can redistribute it and/or modify
 **      it under the terms of the GNU General Public License as published by
