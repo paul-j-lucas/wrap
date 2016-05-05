43e8a26a108d090f579e4c2c590f2ba555f841dd
Paul J. Lucas <plucas@splunk.com>
2016-05-04 19:05:47 -0700

    Many more wrapc Markdown tests.

---
+++ test/data/md-html-05.c
@@ -0,0 +1,7 @@
+  // This is the first sentence.
+  // This is the second sentence.
+  // This is the third sentence.
+  // <em>Span-level HTML.</em>
+  // This is the fourth sentence.
+  // This is the fifth sentence.
+  // This is the sixth sentence.
---
+++ test/data/md-nested-02s.c
@@ -0,0 +1,5 @@
+// 1. First item of outer list.
+//
+//     + First item of nested list.
+//
+// 9. Second item of outer list.
