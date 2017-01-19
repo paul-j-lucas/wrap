4b24cfbf39f389881de481a7f071272ab887419a
Paul J. Lucas <plucas@splunk.com>
2017-01-19 15:51:20 -0800

    Fixed definition of free_now().

--- src/util.c
+++ src/util.c
@@ -134,7 +134,7 @@ void* free_later( void *p ) {
   return p;
 }
 
-void free_now() {
+void free_now( void ) {
   for ( free_node_t *p = free_head; p; ) {
     free_node_t *const next = p->fn_next;
     free( p->fn_ptr );
