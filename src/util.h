c889ca0b54fc018f770d8323db9b88f76141f732
Paul J. Lucas <paul@lucasmail.org>
2024-01-10 17:41:48 -0800

    Better handling of IS_SAME_TYPE().

--- src/util.h
+++ src/util.h
@@ -41,12 +41,16 @@
 #include <string.h>                     /* for strspn(3) */
 #include <sysexits.h>
 
-/// @endcond
-
 _GL_INLINE_HEADER_BEGIN
 #ifndef W_UTIL_H_INLINE
 # define W_UTIL_H_INLINE _GL_INLINE
 #endif /* W_UTIL_H_INLINE */
+
+#if defined(HAVE___BUILTIN_TYPES_COMPATIBLE_P) && defined(HAVE___TYPEOF__)
+# define WITH_IS_SAME_TYPE
+#endif
+
+/// @endcond
 
 /**
  * @defgroup util-group Utility Macros & Functions
@@ -326,7 +330,11 @@ _GL_INLINE_HEADER_BEGIN
  *
  * @sa #IS_POINTER()
  */
-#define IS_ARRAY(A)               !IS_SAME_TYPE( (A), &(A)[0] )
+#ifdef WITH_IS_SAME_TYPE
+# define IS_ARRAY(A)              !IS_SAME_TYPE( (A), &(A)[0] )
+#else
+# define IS_ARRAY(A)              1
+#endif /* WITH_IS_SAME_TYPE */
 
 /**
  * Checks (at compile-time) whether \a P is a pointer.
@@ -336,7 +344,11 @@ _GL_INLINE_HEADER_BEGIN
  *
  * @sa #IS_ARRAY()
  */
-#define IS_POINTER(P)             IS_SAME_TYPE( (P), &(P)[0] )
+#ifdef WITH_IS_SAME_TYPE
+# define IS_POINTER(P)            IS_SAME_TYPE( (P), &(P)[0] )
+#else
+# define IS_POINTER(P)            1
+#endif /* WITH_IS_SAME_TYPE */
 
 /**
  * Checks (at compile-time) whether \a T1 and \a T2 are the same type.
@@ -346,12 +358,10 @@ _GL_INLINE_HEADER_BEGIN
  * @return Returns 1 (true) only if \a T1 and \a T2 are the same type; 0
  * (false) otherwise.
  */
-#if defined(HAVE___BUILTIN_TYPES_COMPATIBLE_P) && defined(HAVE___TYPEOF__)
+#ifdef WITH_IS_SAME_TYPE
 # define IS_SAME_TYPE(T1,T2) \
     __builtin_types_compatible_p( __typeof__(T1), __typeof__(T2) )
-#else
-# define IS_SAME_TYPE(T1,T2)      1
-#endif
+#endif /* WITH_IS_SAME_TYPE */
 
 #ifdef HAVE___BUILTIN_EXPECT
 
