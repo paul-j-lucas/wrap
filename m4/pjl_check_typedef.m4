6345b7d5a93c6d86cff1f8d047885f9e7fbf2630
Paul J. Lucas <paul@lucasmail.org>
2024-03-03 05:37:49 -0800

    Updated copyright year.

--- m4/pjl_check_typedef.m4
+++ m4/pjl_check_typedef.m4
@@ -11,7 +11,7 @@
 #
 # LICENSE
 #
-#     Copyright (C) 2019-2020  Paul J. Lucas
+#     Copyright (C) 2019-2024  Paul J. Lucas
 #
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
--- src/doxygen.c
+++ src/doxygen.c
@@ -2,7 +2,7 @@
 **      wrap -- text reformatter
 **      src/doxygen.c
 **
-**      Copyright (C) 2018-2023  Paul J. Lucas
+**      Copyright (C) 2018-2024  Paul J. Lucas
 **
 **      This program is free software: you can redistribute it and/or modify
 **      it under the terms of the GNU General Public License as published by
